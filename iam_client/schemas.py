a090704176bc4bd2dde992f97e9a2584653f820f
p <p.b.hilale@gmail.com>
2025-12-29 09:49:41 +0530

    added more params in user detail response

--- iam_client/schemas.py
+++ iam_client/schemas.py
@@ -1,15 +1,18 @@
 from pydantic import BaseModel
 from typing import List
 import uuid
+from datetime import datetime
 
 
 class IAMUser(BaseModel):
     id: uuid.UUID
     tenant_id: uuid.UUID
+    full_name: str | None
     email: str | None
     phone: str | None
     roles: List[str]
     is_active: bool
+    created_at: datetime
 
 
 class TokenIntrospection(BaseModel):
